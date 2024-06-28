bebce27becd7a6184bca4508b979918ad56f3323
Suchit G <suchitg04@gmail.com>
2024-06-28 11:38:45 +0530

    Change domain again

--- app/routes/user.py
+++ app/routes/user.py
@@ -93,9 +93,8 @@ async def get_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()], 
         key="token",
         value=access_token,
         expires=datetime.now(timezone.utc) + access_token_expires,
-        secure=True,
         samesite=None,
-        domain="http://localhost:5173",
+        domain="lostip.ddns.net",
     )
     return Token(access_token=access_token, token_type="bearer")
 
